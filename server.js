dacb39f7e62f58a1fd6509f1321e7891f56bb612
Heryson Andrade <141951314+ribshow@users.noreply.github.com>
2024-11-18 15:43:36 -0300

    fix(server.js)

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ import express from "express";
 
 import book from "./book.js";
 
+// instanciando o servidor
 const app = express();
 
+// dizendo em que porta o servidor deve ouvir
 app.listen(3000, () => {
   console.log("Servidor escutando...");
 });
 
+// criando uma rota para nosso "recurso" localmente seria algo do tipo http://localhost:3000/api
 app.get("/api", (req, res) => {
   res.status(200).send("Bem-vindo à imersão alura!");
 });
