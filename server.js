93efdc1cfa10910116e57c789112310c75bd8bac
Heryson Andrade <141951314+ribshow@users.noreply.github.com>
2024-11-19 16:00:48 -0300

    feat(exercise_luri)

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ app.get("/posts", (req, res) => {
   res.status(200).json(posts);
 });
 
+// função para filtrar um post específico pelo id
 const findPost = (id) => {
   return posts.findIndex((post) => {
     return post.id === Number(id);
@@ -57,7 +58,28 @@ app.get(`/posts/:id`, (req, res) => {
   res.status(200).json(posts[index]);
 });
 
-// exercício sugerido pela Luri
+// exercício sugerido pela Luri aula 1
 app.get("/book", (req, res) => {
   res.status(200).send(book);
+});
+
+// exercício sugerido pela Luri aula 2
+
+const findPostforDescription = (description) => {
+  // removendo os espaços e aplicando lowerCase
+  const descriptionNormalized = description.replace(/\s+/g, "").toLowerCase();
+  return posts.filter((post) => {
+    // removendo espaços e aplicando lowercase
+    const normalizedPostDescription = post.description
+      .replace(/\s+/g, "")
+      .toLowerCase();
+
+    // comparando as strings normalziadas
+    return normalizedPostDescription === descriptionNormalized;
+  });
+};
+
+app.get("/post/:description", (req, res) => {
+  const index = findPostforDescription(req.params.description);
+  res.status(200).json(index);
 });
